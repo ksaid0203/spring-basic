e310f0d01ca2195db3e566c952e0efa8905c595b
JUHO KIM <ksaid0203@gmail.com>
2022-07-05 14:02:53 +0900

    Component scan, Dependency Injection Automatically

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>(); // 동시성 이슈 때문에 Concurrency HashMap을 실무에서 사용
